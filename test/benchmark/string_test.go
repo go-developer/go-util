4a79d9aa4730e125e99ad726992a5b144e5fb729
张德满 <go_developer@163.com>
2019-10-08 15:00:22 +0800

    增加一个基准测试的demo

---
+++ test/benchmark/string_test.go
@@ -0,0 +1,24 @@
+package benchmark
+
+import (
+	"go-util/util"
+	"testing"
+)
+
+/**
+ * 测试字符串按照指定分隔符转化为小驼峰的性能
+ * @author go_developer@163.com
+ */
+func BenchmarkToHump(b *testing.B) {
+
+	b.StopTimer() //调用该函数停止压力测试的时间计数
+
+	//做一些初始化的工作，例如读取文件数据，数据库连接之类的，
+	//这样这些时间不影响我们测试函数本身的性能
+
+	b.StartTimer() //重新开始时间
+	for i := 0; i < b.N; i++ { //use b.N for looping
+		util.StringUtil.ToHump("zhang_de_man", "_")
+	}
+}
+
