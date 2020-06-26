b6f435c8f7ad2ecf43e9c06503e70a8bd436b436
张德满 <go_developer@163.com>
2020-06-26 22:44:54 +0800

    [master]优化获取项目根目录

--- util/project.go
+++ util/project.go
@@ -11,8 +11,6 @@ package util
 
 import (
 	"os"
-	"os/exec"
-	"strings"
 )
 
 // ProjectUtil 项目相关工具集
@@ -29,11 +27,5 @@ type projectUtil struct {
 //
 // Date : 2020/06/26 20:53:06
 func (pu *projectUtil) GetCurrentPath() (string, error) {
-	s, err := exec.LookPath(os.Args[0])
-	if nil != err {
-		return "", err
-	}
-	i := strings.LastIndex(s, "\\")
-	path := string(s[0 : i+1])
-	return path, nil
+	return os.Getwd()
 }
